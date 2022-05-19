be28235049a1ec4aad8fec7923e49b78ac076fcf
ahah525 <ahah525@naver.com>
2022-05-19 19:59:04 +0900

    7. 의존관계 자동 주입 - @Primary

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -3,10 +3,11 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")    // 추가 구분자
+@Primary    // 우선권 부여
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;    // 할인율(10%)
