7b6bd6fe38c46298894c8fee7b440a6fc626d16f
ahah525 <ahah525@naver.com>
2022-05-24 18:07:03 +0900

    9. 빈 스코프 - 스코프와 Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,17 +15,21 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;  // myLogger 를 찾을 수 있는 ObjectProvider 주입
     
     // log-demo 요청이 오면 웹 브라우저에 데이터를 문자로 반환
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         // HttpServletRequest: 클라이언트 요청 정보
         String requestURL = request.getRequestURL().toString(); // 클라이언트가 요청한 URL 반환
+        // ObjectProvider로 myLogger 조회(DL)
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL); // myLogger 의 requestURL 에 값 저장
 
+        //myLogger.log("LogDemoController-MyLogger : " + myLogger);
         myLogger.log("controller test");
+        //Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
